66a4238c8a6efd50decf5b4488d23a24c5e0f9ea
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2021-12-20 21:30:31 +0530

    deleted

--- Student-Database-Management-System-using-Flask-main/StudentDB.py
+++
@@ -1,9 +0,0 @@
-import cx_Oracle as c
-connection = c.connect("c##scott/tiger@localhost/XE")
-print("Database opened successfully")
-cursor = connection.cursor()
-#delete
-#cursor.execute('''DROP TABLE Student_Info;''')
-connection.execute("create table Student_Info (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT UNIQUE NOT NULL, gender TEXT NOT NULL, contact TEXT UNIQUE NOT NULL, dob TEXT NOT NULL, address TEXT NOT NULL)")
-print("Table created successfully")
-connection.close()   
